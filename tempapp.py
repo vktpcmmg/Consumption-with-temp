bc68aa307c88d9881dcca0bced765861d7a1e3df
vktpcmmg <vk340387@gmail.com>
2025-07-20 13:43:36 +0530

    Update tempapp.py

--- tempapp.py
+++ tempapp.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 import base64
-from openai import OpenAI
 import requests
 
 # ------------ LOGO DISPLAY ------------
